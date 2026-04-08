cb4ddbfd7c9f2c3036e4c7aafb719e86711e30f1
GerardoOrgg <20253ds064@utez.edu.mx>
2026-04-08 00:11:39 -0600

    Controllers empezados

--- Integradora_Inventario/src/main/java/com/example/integradora_inventario/ProductoService/ProductoService.java
+++ Integradora_Inventario/src/main/java/com/example/integradora_inventario/ProductoService/ProductoService.java
@@ -1,4 +1,127 @@
 package com.example.integradora_inventario.ProductoService;
 
+import com.example.integradora_inventario.Model.Producto;
+import com.example.integradora_inventario.ProductoFile.ProductoFile;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductoService {
+
+
+    ProductoFile file = new ProductoFile();
+
+    public List<Producto> loadForListView() throws IOException {
+        List<String> lines = file.readAllLines();
+        List<Producto> result = new ArrayList<>();
+
+        for (String line : lines) {
+            if (line == null || line.isBlank()) continue;
+            String[] parts = line.split("-");
+            if (parts.length < 5) {
+                System.out.println("Línea inválida (faltan datos): " + line);
+                continue;
+            }
+            try {
+                String codigo = parts[0];
+                String nombre = parts[1];
+                double precio = Double.parseDouble(parts[2]);
+                int stock = Integer.parseInt(parts[3]);
+                String categoria = parts[4];
+                result.add(new Producto(codigo, nombre, precio, stock, categoria));
+
+            } catch (NumberFormatException e) {
+                System.out.println("Error de formato numérico en la línea: " + line);
+            }
+        }
+        return result;
+    }
+
+    public void Agregar(String codigo, String nombre, String precio, String stock, String categoria) throws IOException {
+        file.addNewLine(codigo + "-" + nombre + "-" + precio + "-" + stock + "-" + categoria);
+    }
+
+    public void editarProducto(String codigo, String nombre, String precio, String stock, String categoria) throws IOException {
+        List<String> todasLasLineas = file.readAllLines();
+        List<String> nuevaLista = new ArrayList<>();
+
+        for (String linea : todasLasLineas) {
+            if (linea == null || linea.isBlank()) continue;
+
+            String[] parts = linea.split("-");
+            if (parts[0].equals(codigo)) {
+                nuevaLista.add(codigo + "-" + nombre + "-" + precio + "-" + stock + "-" + categoria);
+            } else {
+                nuevaLista.add(linea);
+            }
+        }
+        file.saveFile(nuevaLista);
+    }
+
+
+    public void eliminarProducto(String item) throws IOException {
+        String[] parts = item.split("-");
+        String Cod = parts[0];
+        file.eliminarPorCodigo(Cod);
+    }
+
+
+    public String validarDatos(String codigo, String nombre, String precio, String stock, String categoria) throws IOException {
+        Boolean codigorepetido = file.BuscarCodigo(codigo);
+        if (codigorepetido) {return "El codigo esta repetido";}
+        if (codigo == null || codigo.isBlank()) {return "El codigo esta vacio";}
+        if (nombre == null || nombre.isBlank()) {return "El nombre esta vacio";}
+        if (nombre.length() < 3) {return "El nombre es muy corto";}
+        if (precio.isBlank()) {return "El precio esta vacio";}
+        if (stock.isBlank()) {return "El stock esta vacio";}
+        if (categoria == null || categoria.isBlank()) {return "La categoria esta vacio";
+        }
+
+        try {
+            double preVal = Double.parseDouble(precio);
+            int stockVal = Integer.parseInt(stock);
+
+            if (preVal < 0) return "El precio no puede ser menor a 0";
+            if (stockVal < 0) return "El stock no puede ser menor a 0";
+
+        } catch (NumberFormatException e) {
+            return "El precio o stock deben ser valores numéricos";
+        }
+        return "Producto agregado";
+
+    }
+
+    public String validarEditar(String codigo, String nombre, String precio, String stock, String categoria) throws IOException {
+        Boolean codigorepetido = file.BuscarCodigo(codigo);
+        if (codigorepetido) {return "El codigo esta repetido";}
+        if (codigo == null || codigo.isBlank()) {
+            return "El codigo esta vacio";
+        }
+        if (nombre == null || nombre.isBlank()) {
+            return "El nombre esta vacio";
+        }
+        if (nombre.length() < 3) {
+            return "El nombre es muy corto";
+        }
+        if (precio.isBlank()) {
+            return "El precio esta vacio";
+        }
+        if (stock.isBlank()) {
+            return "El stock esta vacio";
+        }
+        if (categoria == null || categoria.isBlank()) {
+            return "La categoria esta vacio";
+        }
+        try {
+            double preVal = Double.parseDouble(precio);
+            int stockVal = Integer.parseInt(stock);
+
+            if (preVal < 0) return "El precio no puede ser menor a 0";
+            if (stockVal < 0) return "El stock no puede ser menor a 0";
+
+        } catch (NumberFormatException e) {
+            return "El precio o stock deben ser valores numéricos";
+        }
+        return "Producto Editado Correctamente";
+    }
 }
--- Integradora_Inventario/src/main/java/module-info.java
+++ Integradora_Inventario/src/main/java/module-info.java
@@ -1,8 +1,20 @@
-module com.example.integradora_inventario {
+module com.example.integradora {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.integradora_inventario to javafx.fxml;
     exports com.example.integradora_inventario;
+
+    opens com.example.integradora_inventario.ProductoService to javafx.fxml;
+    exports com.example.integradora_inventario.ProductoService;
+
+    opens com.example.integradora_inventario.Model to javafx.fxml;
+    exports com.example.integradora_inventario.Model;
+
+    opens com.example.integradora_inventario.Controllers to javafx.fxml;
+    exports com.example.integradora_inventario.Controllers;
+
+
 }
